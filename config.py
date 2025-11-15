4d549fb1f0270c8cd751cfa449e87f64d27ca8ff
Thomas Lahore <tom.lahore@gmail.com>
2025-11-14 16:44:47 -0800

    misc

--- config.py
+++ config.py
@@ -224,6 +224,6 @@ CURRENT_PAGE_ID = None  # set during full conversion for debug context
 CURRENT_PAGE_NAME = None  # set during full conversion for debug context
 
 # whitelist_by_slug = [
-#     'vitamin-d-presentation-charts'
+#     'virus'
 # ]
 whitelist_by_slug = None
